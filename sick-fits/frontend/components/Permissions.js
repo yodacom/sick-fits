72428200ee37a129802993f407592ddced6e5dd7
Jeremy Black <jb@yodacom.com>
2018-12-08 17:37:07 -0700

    auth Admin and begining permissions

---
+++ sick-fits/frontend/components/Permissions.js
@@ -0,0 +1,28 @@
+import { Query } from 'react-apollo';
+import Error from './ErrorMessage';
+import gql from 'graphql-tag';
+
+const ALL_USERS_QUERY = gql`
+  query {
+    users {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`;
+
+const Permissions = (props) => (
+  <Query query={ALL_USERS_QUERY}>
+    {({ data, loading, error }) =>
+      console.log(data) || (
+        <div>
+          <Error error={error} />
+          <p>Hey</p>
+        </div>
+      )}
+  </Query>
+);
+
+export default Permissions;
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,8 +1,11 @@
-import Link from 'next/link';
+import CreateItem from '../components/CreateItem';
+import PleaseSignIn from '../components/PleaseSignIn';
 
 const Sell = props => (
   <div>
-    <p>Sell!</p>
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
   </div>
 );
 
